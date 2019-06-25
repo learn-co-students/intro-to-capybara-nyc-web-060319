130a94646f4077c59284470594119a2805386ce5
TimeSmash <TimeSmash@github.com>
2019-06-24 21:37:18 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 class Application < Sinatra::Base
   # Write your code here!
 
+  get '/' do
+    erb :index
+  end
+
+  post '/greet' do #This is saying that if POST is used on greet, display the greet.erb file contents to the user
+    erb :greet
+  end
 end
